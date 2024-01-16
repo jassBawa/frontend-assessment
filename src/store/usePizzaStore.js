e5d10012d0f48dd6f24448658156c9abb14608a8
jassBawa <jasspreetsbawa@gmail.com>
2024-01-16 23:42:48 +0530

    fix: removed devtools

--- src/store/usePizzaStore.js
+++ src/store/usePizzaStore.js
@@ -1,26 +1,21 @@
 import { create } from "zustand";
-import zukeeper from "zukeeper";
 
 import pizzaData from "../data/pizza.json";
 
 // Zustand store for managing state
-const usePizzaStore = create(
-  zukeeper((set) => ({
-    selectedTab: null,
-    setSelectedTab: (newTab) => set({ selectedTab: newTab }),
-    jsonData: pizzaData,
-    setJsonData: (newJsonData) => set({ jsonData: newJsonData }),
-    formFields: {},
-    updateFormField: (fieldName, value) =>
-      set((state) => ({
-        formFields: {
-          ...state.formFields,
-          [fieldName]: value,
-        },
-      })),
-  }))
-);
-
-window.store = usePizzaStore;
+const usePizzaStore = create((set) => ({
+  selectedTab: null,
+  setSelectedTab: (newTab) => set({ selectedTab: newTab }),
+  jsonData: pizzaData,
+  setJsonData: (newJsonData) => set({ jsonData: newJsonData }),
+  formFields: {},
+  updateFormField: (fieldName, value) =>
+    set((state) => ({
+      formFields: {
+        ...state.formFields,
+        [fieldName]: value,
+      },
+    })),
+}));
 
 export default usePizzaStore;
